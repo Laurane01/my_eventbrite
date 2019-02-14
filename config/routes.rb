a42dd39bc82fdbef4183c9277a319233930842e6
Laurane01 <petitlaurane01@gmail.com>
2019-02-14 01:39:17 +0100

    commit encore

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,9 @@ get "events/subscribe/:id", to: "attendances#subscribe", as:"attendance_subscrip
 #get "events/:id/:id", to: "events#adduser"
 post "events/subscribe/:id", to: "attendances#subscribe"
 
-get '/login', to: 'session#new'
-post '/login', to: 'session#create'
-delete '/logout', to: 'session#destroy'
+get '/login', to: 'sessions#new'
+post '/login', to: 'sessions#create'
+delete '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
